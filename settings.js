cd554e35710ded1ea74b7cc4970e5bf782d76b3e
samzmarket <acilepep15@gmail.com>
2024-09-14 15:10:16 +0700

    bebas

---
+++ settings.js
@@ -0,0 +1,134 @@
+/*
+
+     
+    👇👇👇 𝙱𝙰𝙲𝙰 𝙸𝙽𝙸 𝙿𝙴𝙽𝚃𝙸𝙽𝙶 !! 👇👇👇
+
+ * Script :  Made By Anggazyy ❤😁
+ * Script : Di lindungi dengan ketat !
+ * Note : This script not for free, hargai creator.
+ * Thanks To ❤ : Saqioo, Hanmood, Anggazyy, Zaenshi 
+ * Terimakasih telah berkontribusi di script ini :)
+ * Owner utama : https://t.me/anggazyydev
+ * Saluran Saya : https://whatsapp.com/channel/0029VakZidSIXnlmikWIgS1z
+ * Script Security ( Anti Air Bug )
+ 
+ * Attention : terimakasi telah membeli script devil ini
+              semoga kalian betah pakai script ini
+              jangan pernah beli ke orang cukup
+              beli ke owner langsung (anggazyydev)
+ *Thank You...
+ 
+
+*/
+require("./all/module.js")
+const { color } = require('./all/function')
+const version = require("@whiskeysockets/baileys/package.json").version
+//========== Setting Owner ==========//
+global.owner = "6289507318696"
+global.namaowner = "𝘚𝘈𝘔𝘡 𝘔𝘈𝘙𝘒𝘌𝘛"
+global.namaowner2 = "𝘚𝘈𝘔𝘡 𝘔𝘈𝘙𝘒𝘌𝘛"
+
+//======== Setting Bot & Link ========//
+global.namabot = "𝘚𝘈𝘔𝘡 𝘉𝘖𝘛𝘡" 
+global.botname = "𝘚𝘈𝘔𝘡 𝘉𝘖𝘛𝘡"
+global.namabot2 = "𝘚𝘈𝘔𝘡 𝘉𝘖𝘛𝘡"
+global.namaowner = "𝘋𝘦𝘷𝘪𝘭 𝘚𝘤𝘳𝘪𝘱𝘵 - 𝘉𝘺 𝘚𝘈𝘔𝘡 𝘔𝘈𝘙𝘒𝘌𝘛" // ini untuk funticon reply dll/respon cmd
+global.linkfoto = "https://telegra.ph/file/d6d496e06cbf1ffca015c.jpg" // untuk foto di cmd menu/spesialmenu/bugmenu
+global.version = "SAMZ MARKET"
+global.foother = "©SAMZ MARKET"
+global.idsaluran = "120363301756685796@newsletter"
+global.linkgc = 'https://chat.whatsapp.com/IEzQWqzSqwbGt4iKWKHzcq'
+global.linksaluran = "https://whatsapp.com/channel/0029Vao7z6YEAKW85wH63A2L"
+global.linkyt = 'https://www.youtube.com/@AMZYYOP'
+global.linktele = "https://t.me/samlvlup"
+global.packname = "Created By Samz Market"
+global.author = "SAMZ MARKET"
+global.newsletterName = "SamzBotz"
+global.target = 'https://www.en.sfcnavy.gov.bd/public/files/assets/ckeditor/kcfinder/upload.php'
+global.premium = ["6285704356559"] // Premium User
+global.wm = "By Samz - Devil Script"
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+
+//========== Setting Event ==========//
+global.welcome = false
+global.autoread = false
+global.anticall = false
+global.autoreadsw = false
+global.owneroff = false
+global.antibug = true
+
+//==== Waktu Jeda Jpm & Pushkon ====//
+global.delaypushkontak = 9000
+global.delayjpm = 6000
+
+//========== Setting Foto ===========//
+
+//========== Setting Panell & Gausah isi kalo gadaa ==========//
+//========== Setting Panell & Gausah isi kalo gadaa ==========//
+global.egg = "15"
+global.loc = "1"
+global.domain = ""
+global.apikey = ""
+global.capikey = ""
+
+global.fake = {
+	anonim: 'https://telegra.ph/file/f63511c4304de908fa5b0.jpg',
+	thumbnailUrl: 'https://telegra.ph/file/d6d496e06cbf1ffca015c.jpg',
+	thumbnail: fs.readFileSync('./media/anggazyy.jpg'),
+	docs: fs.readFileSync('./media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+    ch: 'https://whatsapp.com/channel/0029Vao7z6YEAKW85wH63A2L',
+	gh: 'https://chat.whatsapp.com/IEzQWqzSqwbGt4iKWKHzcq',
+	
+}
+//========= Setting Payment =========//
+//Kalo Gak Ada Isi Aja jadi false
+global.dana = "082353618930"
+global.gopay = "082334292520"
+global.ovo = false
+global.qris = fs.readFileSync("./media/qris.jpg")
+                             
+//=========== Api Domain ===========//
+global.zone1 = "c2047082b74a80e5be03959bad59592a"
+global.apitoken1 = "Nop2RDsy0Uyh1WHE17CC59aEhen-ZA61MWNrAqVl"
+global.tld1 = "digitalserver.biz.id"
+
+//========== Api Domain 2 ==========//
+global.zone2 = "a476ffcf9243c44a02220f184da527e8";
+global.apitoken2 = "RsbJAI6X7s7bPEj23R7sf28cqHibApP1EBSoF4FZ";
+global.tld2 = "mypanell.biz.id";
+//========== Api Domain 3 ==========//
+global.zone3 = "5f4a582dd80c518fb2c7a425256fb491";
+global.apitoken3 = "iQbJQgfe6kTyEfdOy_EV8UAHKj80VgQg4t6rTjby";
+global.tld3 = "tokopanellku.my.id";
+//========== Api Domain 4 ==========//
+global.zone4 = "d41a17e101c0f89f0aec609c31137f91";
+global.apitoken4 = "miC4wpso1vMcRFR62ZvOFfFd9xTlawvHcXPYZgwi";
+global.tld4 = "panellstore.net";
+
+//========= Setting Message =========//
+global.msg = {
+"error": "Error! terjadi kesalahan.",
+"done": "Donee, ~ Oke desuuu.", 
+"wait": "Please wait a minute bot being proses.", 
+"group": "*• Group Only* Fitur Ini Hanya Untuk Di Dalam Grup!", 
+"private": "*• Private Chat* Fitur Ini Hanya Untuk Didalam Private Chat!", 
+"anggazyyprem": "*~ Devil System:*\n\n- Khusus user premium sayang.",
+"admin": "*• Admin Only* Fitur Ini Hanya Untuk Admin Grup!", 
+"adminbot": "*• Bot Admin* Fitur Ini Dapat Digunakan Ketika Bot Menjadi Admin", 
+"owner": "*Khusus owner sayang*", 
+"query": "*Masukan query*",
+"developer": "*• Developer Only* Fitur Ini Hanya Untuk Developer"
+}
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
